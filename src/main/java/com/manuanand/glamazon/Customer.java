073bf54f729dcd5ba5e45481332a3ce3bce26e38
Manu Anand <mananan@microsoft.com>
2021-07-10 15:31:28 +0530

    Initial commit of Glamazon shopping app

---
+++ src/main/java/com/manuanand/glamazon/Customer.java
@@ -0,0 +1,96 @@
+package com.manuanand.glamazon;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+
+	private String name;
+	
+	private Date birthDate;
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	private String email;
+	
+	private String password;
+	
+	private String phone;
+
+	private String address;
+	
+	// Comma separated list of categories
+	private String preferredCateories;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPreferredCateories() {
+		return preferredCateories;
+	}
+
+	public void setPreferredCateories(String preferredCateories) {
+		this.preferredCateories = preferredCateories;
+	}
+	
+}
---
+++ src/main/java/com/manuanand/glamazon/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.manuanand.glamazon;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.manuanand.glamazon.Customer;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+}
